b6c769056bdd10db661ac587d8f78c85a58e636d
Awinja-j <ingari61@hotmail.com>
2022-06-08 23:34:25 +0300

    new code

---
+++ app.py
@@ -0,0 +1,54 @@
+import os
+import json
+from src.src import WhatsApp
+from dotenv import load_dotenv
+from flask import Flask, request
+
+
+app = Flask(__name__)
+
+# Load .env file
+load_dotenv()
+
+messenger = WhatsApp(os.getenv("TOKEN"))
+VERIFY_TOKEN = os.environ("VERIFY_TOKEN")
+
+
+@app.route("/", methods=["GET", "POST"])
+def webhook():
+    if request.method == "GET":
+        if request.args.get("hub.verify_token") == VERIFY_TOKEN:
+            return request.args.get("hub.challenge")
+        return "Invalid verification token"
+
+    data = request.get_json()
+    changed_field = messenger.changed_field(data)
+    if changed_field == "messages":
+        new_message = messenger.get_mobile(data)
+        if new_message:
+            mobile = messenger.get_mobile(data)
+            message_type = messenger.get_message_type(data)
+
+            if message_type == "text":
+                message = messenger.get_message(data)
+                name = messenger.get_name(data)
+                print(f"{name} with this {mobile} number sent  {message}")
+                messenger.send_message(f"Hi {name}, nice to connect with you", mobile)
+
+            elif message_type == "interactive":
+                message_response = messenger.get_interactive_response(data)
+                print(message_response)
+
+            else:
+                pass
+        else:
+            delivery = messenger.get_delivery(data)
+            if delivery:
+                print(f"Message : {delivery}")
+            else:
+                print("No new message")
+    return "ok"
+
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
